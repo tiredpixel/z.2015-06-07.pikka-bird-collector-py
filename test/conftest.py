9016bec0666d288db5212a3f3f166cd2ddb63c32
tiredpixel <tp@tiredpixel.com>
2015-04-19 15:16:54 +0100

    Test Collector,Sender , installing httmock.

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,24 @@
 import os
 if os.environ.get('CI') != '1':
     exit("!!! CI environment variable must be set for testing environment")
+
+import pytest
+
+
+@pytest.fixture
+def collection_valid():
+    return {
+        'collected_at':  '2015-04-04T19:32:20.616977',
+        'collecting_at': '2015-04-04T19:33:01.424242',
+        'environment':   {
+            'hostname': 'localhost',
+            'pid':      42,
+            'version':  '1.2.3',
+            'platform': {
+                'system':  "Hovercraft",
+                'release': "42.21.11",
+                'version': "Hovercraft Kernel Version 42.21.11 128-bit"}},
+        'reports': {
+            'system': {
+                'load': {
+                    'avg_15_min': 1.62939453125}}}}
---
+++ test/test_sender.py
@@ -0,0 +1,73 @@
+from httmock import urlmatch, HTTMock
+
+from pikka_bird_collector.sender import Sender
+
+
+SERVER_HOST = 'pikka-bird.example.com'
+SERVER_URI  = 'http://%s' % SERVER_HOST
+
+
+@urlmatch(netloc=SERVER_HOST, path='/collections', method='post')
+def mock_collections_post_201(url, request):
+    return {
+        'status_code': 201,
+        'content':     {}}
+
+@urlmatch(netloc=SERVER_HOST, path='/collections', method='post')
+def mock_collections_post_204(url, request):
+    return {
+        'status_code': 204}
+
+@urlmatch(netloc=SERVER_HOST, path='/collections', method='post')
+def mock_collections_post_422(url, request):
+    return {
+        'status_code': 422,
+        'content':     {}}
+
+@urlmatch(netloc=SERVER_HOST, path='/collections', method='post')
+def mock_collections_post_500(url, request):
+    return {
+        'status_code': 500,
+        'content':     'very very broken'}
+
+
+class TestSender:
+    
+    def test_send(self, collection_valid):
+        sender = Sender(SERVER_URI)
+        
+        with HTTMock(mock_collections_post_201):
+            r = sender.send(collection_valid)
+        
+        assert r == True
+    
+    def test_send_204(self, collection_valid):
+        sender = Sender(SERVER_URI)
+        
+        with HTTMock(mock_collections_post_204):
+            r = sender.send(collection_valid)
+        
+        assert r == True
+    
+    def test_send_422(self, collection_valid):
+        sender = Sender(SERVER_URI)
+        
+        with HTTMock(mock_collections_post_422):
+            r = sender.send(collection_valid)
+        
+        assert r == False
+    
+    def test_send_500(self, collection_valid):
+        sender = Sender(SERVER_URI)
+        
+        with HTTMock(mock_collections_post_500):
+            r = sender.send(collection_valid)
+        
+        assert r == False
+    
+    def test_send_connection_failed(self, collection_valid):
+        sender = Sender('http://localhost:666')
+        
+        r = sender.send(collection_valid)
+        
+        assert r == False
