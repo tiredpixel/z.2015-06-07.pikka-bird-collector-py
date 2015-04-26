fdc88f74c28be52553f5e7aa266bc9ca9b7c0bbe
tiredpixel <tp@tiredpixel.com>
2015-04-26 15:47:49 +0100

    Package, using setuptools-git.

--- setup.py
+++ setup.py
@@ -8,16 +8,33 @@ import pikka_bird_collector
 setup(
     name     = 'pikka-bird-collector',
     version  = pikka_bird_collector.__version__,
-    packages = find_packages(),
-    scripts  = ['bin/pikka-bird-collector'],
+    
+    include_package_data = True,
+    packages             = find_packages(),
+    scripts              = [
+        'bin/pikka-bird-collector'],
     
     author       = 'tiredpixel',
     author_email = 'tp@tiredpixel.com',
+    classifiers  = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
     description  = "Pikka Bird ops monitoring tool Collector component.",
+    keywords     = 'ops monitoring sysadmin',
     license      = 'MIT',
-    keywords     = 'monitoring',
     url          = 'https://github.com/tiredpixel/pikka-bird-collector-py',
     
     install_requires = [
+        'msgpack-python',
+        'psutil',
+        'requests',
     ]
 )
